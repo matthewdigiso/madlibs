cfaf985c004399e2fbdb17e21c403bab2e23da5a
MATTHEW DIGISO <126MDIGISO@FRHSDCOM>
2025-02-10 13:59:59 -0500

    commit and push changes

--- js/main.js
+++ js/main.js
@@ -1,17 +1,21 @@
-document.getElementById('madLibForm').addEventListener('submit', function(e) {
-    e.preventDefault();
-
-    // Get values from the form
-    const adjective = document.getElementById('adjective').value;
-    const noun = document.getElementById('noun').value;
-    const verb = document.getElementById('verb').value;
-    const place = document.getElementById('place').value;
-
-    // Generate the Mad Lib story
-    const madLib = `The ${adjective} ${noun} decided to ${verb} at the ${place}.`;
-
-    // Display the result and hide the form
-    document.getElementById('madLibResult').textContent = madLib;
-    document.getElementById('resultSection').style.display = 'block';
-    document.getElementById('madLibForm').style.display = 'none';
-});
+// JavaScript FILE (main.js)
+function generateStory() {
+    let inputs = document.querySelectorAll(".madlib-input");
+    let story = document.getElementById("story");
+    let filledStory = `A vacation is when you take a trip to some ${inputs[0].value} place
+    with your ${inputs[1].value} family. Usually, you go to some place
+    that is near a/an ${inputs[2].value} or up on a/an ${inputs[3].value}.
+    A good vacation place is one where you can ride ${inputs[4].value}
+    or play ${inputs[5].value} or go hunting for ${inputs[6].value}.
+    I like to spend my time ${inputs[7].value} or ${inputs[8].value}.
+    When parents go on vacation, they spend their time eating
+    three ${inputs[9].value} a day, and fathers play golf, and mothers
+    sit around ${inputs[10].value}.
+    Last summer, my little brother fell in a/an ${inputs[11].value}
+    and got poison ${inputs[12].value} all over his ${inputs[13].value}.
+    My family is going to go to the ${inputs[14].value} and I will practice ${inputs[15].value}.
+    Parents need vacations more than kids because parents are always ${inputs[16].value} and ${inputs[17].value}.`;
+    
+    story.innerHTML = `<p>${filledStory}</p>`;
+    story.style.display = "block";
+}
