fa9be622b8994a232cdaeac5518fd2d9f2d57a48
MATTHEW DIGISO <126MDIGISO@FRHSDCOM>
2025-02-12 13:09:56 -0500

    save ndcommit changes

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
-// JavaScript FILE (main.js)
-function generateStory() {
-    let inputs = document.querySelectorAll(".madlib-input");
-    let story = document.getElementById("story");
-    let filledStory = `A vacation is when you take a trip to some ${inputs[0].value} place
-    with your ${inputs[1].value} family. Usually, you go to some place
-    that is near a/an ${inputs[2].value} or up on a/an ${inputs[3].value}.
-    A good vacation place is one where you can ride ${inputs[4].value}
-    or play ${inputs[5].value} or go hunting for ${inputs[6].value}.
-    I like to spend my time ${inputs[7].value} or ${inputs[8].value}.
-    When parents go on vacation, they spend their time eating
-    three ${inputs[9].value} a day, and fathers play golf, and mothers
-    sit around ${inputs[10].value}.
-    Last summer, my little brother fell in a/an ${inputs[11].value}
-    and got poison ${inputs[12].value} all over his ${inputs[13].value}.
-    My family is going to go to the ${inputs[14].value} and I will practice ${inputs[15].value}.
-    Parents need vacations more than kids because parents are always ${inputs[16].value} and ${inputs[17].value}.`;
-    
-    story.innerHTML = `<p>${filledStory}</p>`;
-    story.style.display = "block";
-}
+document.addEventListener("DOMContentLoaded", function () {
+    function generateStory() {
+        const inputs = document.querySelectorAll(".madlib-input");
+        const words = Array.from(inputs).map(input => input.value.trim());
+
+        if (words.some(word => word === "")) {
+            alert("Please fill in all the fields before generating the story!");
+            return;
+        }
+
+        const story = `
+            This summer, I went on a ${words[0]} and ${words[1]} trip to the ${words[2]}. 
+            We packed our ${words[3]} and brought lots of ${words[4]}. 
+            My favorite activity was ${words[5]} with the ${words[6]}. 
+            Every morning, we enjoyed ${words[7]} and ${words[8]}, 
+            followed by collecting ${words[9]}. 
+            In the afternoon, we spent time ${words[10]} near the ${words[11]}. 
+            I even got sunburned on my ${words[12]}! 
+            At night, we explored the ${words[13]} while ${words[14]}. 
+            It was a ${words[15]} and ${words[16]} trip, and I can't wait to go again!`;
+
+        const story
